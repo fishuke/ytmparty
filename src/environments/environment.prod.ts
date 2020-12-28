aad5f160c9dbd8a465be143ec5eb908522727196
Fishuke <gamecalp@gmail.com>
2020-12-28 17:46:58 +0300

    Add CLIENT_ID and WS_URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   ORIGIN: 'http://localhost:4200',
+  CLIENT_ID: 'iorij84zsvsyowclla1vnco2mqaa49',
+  WS_URL: 'wss://irc-ws.chat.twitch.tv:443'
 };
