2b6f43f215e238b02aa7d1ed34d20d23c35bcfea
Fishuke <gamecalp@gmail.com>
2020-12-28 18:52:00 +0300

    Add chat

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../core/services/auth.service';
+import * as tmi from 'tmi.js';
+import {TokenService} from '../../core/services/token.service';
+import {Message} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,14 +10,37 @@ import {AuthService} from '../../core/services/auth.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  messageArray: Message[] = [];
 
-  constructor(private auth: AuthService) {
-
+  constructor(private auth: AuthService, private token: TokenService) {
   }
 
   ngOnInit(): void {
+    this.bootstrap();
+  }
 
-
+  async bootstrap(): Promise<void> {
+    await this.auth.loginPromise();
+    const client = new tmi.Client({
+      connection: {
+        secure: true,
+        reconnect: true
+      },
+      identity: {
+        username: this.auth.userState.name,
+        password: `oauth:${this.token.getToken()}`
+      },
+      channels: [this.auth.userState.name]
+    });
+    client.connect().catch(console.error);
+    client.on('message', (channel, tags, message, self) => {
+      console.log(tags);
+      this.messageArray.push({
+        content: message,
+        author: tags['display-name'],
+        color: tags.color
+      });
+    });
   }
 
 }
