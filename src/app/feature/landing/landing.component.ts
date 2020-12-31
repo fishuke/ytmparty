592bcfdeac4d5505bb319d5e54da2f59a2b314b3
Fishuke <gamecalp@gmail.com>
2020-12-31 14:51:42 +0300

    Update Landing Page

--- src/app/feature/landing/landing.component.ts
+++ src/app/feature/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void {
   }
