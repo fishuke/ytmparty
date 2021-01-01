24c5df82d112d8faae2c46bb06a8754a8cd3fe6e
fishuke <gamecalp@gmail.com>
2021-01-01 15:21:00 +0300

    Fix enviorment.prod origin url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  ORIGIN: 'http://localhost:4200',
+  ORIGIN: 'https://fishuke.github.io/better-chat',
   CLIENT_ID: 'iorij84zsvsyowclla1vnco2mqaa49',
   WS_URL: 'wss://irc-ws.chat.twitch.tv:443'
 };
