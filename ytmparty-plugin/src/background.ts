ed5c45799b47fdea9ed472d9735ad1227e6cc5e8
Fishuke <gamecalp@gmail.com>
2021-01-07 12:14:37 +0300

    Update background

--- ytmparty-plugin/src/background.ts
+++ ytmparty-plugin/src/background.ts
@@ -1,5 +1,18 @@
-chrome.runtime.onMessage.addListener((request, callback) => {
-  console.log(request);
-});
-// chrome.commands.onCommand()
 
+class Background {
+  isInParty: boolean;
+
+  constructor() {
+    this.listenMessages();
+  }
+
+  listenMessages(): void {
+    chrome.runtime.onMessage.addListener((request, callback) => {
+      console.log(request);
+    });
+  }
+
+}
+
+// tslint:disable-next-line:no-unused-expression
+new Background();
