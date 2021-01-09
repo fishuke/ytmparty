7c0bfc397bce788fac184e3b23f7c7fad11cd329
Fishuke <gamecalp@gmail.com>
2021-01-09 18:27:58 +0300

    Update app.component.ts

--- ytmparty-plugin/src/app/app.component.ts
+++ ytmparty-plugin/src/app/app.component.ts
@@ -1,26 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  public IsInParty = false;
+export class AppComponent implements OnInit {
+  public partyCode = new FormControl('', [Validators.maxLength(8), Validators.minLength(8)]);
+  public isInParty;
   public joinedPartyCode;
   public response;
-  public value = '';
   private extensionId = 'oononiaicnkfdebjkpfabepkggkneeep';
 
   constructor() {
+  }
+
+  ngOnInit(): void {
     chrome.runtime.sendMessage(this.extensionId, {event: 'isInParty'},
       response => {
-        console.log(response);
+        this.isInParty = response;
+        if (response) {
+          this.getPartyCode();
+        }
       });
+    setTimeout(() => {
+      chrome.runtime.sendMessage(this.extensionId, {event: 'isInParty'},
+        response => {
+          this.isInParty = response;
+          if (response) {
+            this.getPartyCode();
+          }
+        });
+    }, 50);
   }
 
   async onJoinPartyButtonClick(): Promise<void> {
-    chrome.runtime.sendMessage(this.extensionId, {event: 'joinParty', partyCode: this.value},
+    chrome.runtime.sendMessage(this.extensionId, {event: 'joinParty', partyCode: this.partyCode.value},
       response => {
         console.log(response);
       });
@@ -30,6 +46,21 @@ export class AppComponent {
     chrome.runtime.sendMessage(this.extensionId, {event: 'createParty'},
       response => {
         console.log(response);
+      });
+  }
+
+  async onLeavePartyButtonClick(): Promise<void> {
+    chrome.runtime.sendMessage(this.extensionId, {event: 'leaveParty'},
+      response => {
+        console.log(response);
+      });
+  }
+
+  getPartyCode(): void {
+    chrome.runtime.sendMessage(this.extensionId, {event: 'getPartyCode'},
+      response => {
+        console.log(response);
+        this.joinedPartyCode = response;
       });
   }
 
