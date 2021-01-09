0f96a3c2f6c01a7200fc7897d3352231afe3b3f0
Fishuke <gamecalp@gmail.com>
2021-01-09 17:14:09 +0300

    Add isInParty Check

--- ytmparty-plugin/src/app/app.component.ts
+++ ytmparty-plugin/src/app/app.component.ts
@@ -6,24 +6,29 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public IsInParty: boolean;
   public joinedPartyCode;
   public response;
   public error;
   public value = '';
-  private editorExtensionId = 'oononiaicnkfdebjkpfabepkggkneeep';
+  private extensionId = 'oononiaicnkfdebjkpfabepkggkneeep';
 
   constructor() {
+    chrome.runtime.sendMessage(this.extensionId, {event: 'isInParty'},
+      response => {
+        console.log(response);
+      });
   }
 
   async onJoinPartyButtonClick(): Promise<void> {
-    chrome.runtime.sendMessage(this.editorExtensionId, {event: 'joinParty', partyCode: this.value},
+    chrome.runtime.sendMessage(this.extensionId, {event: 'joinParty', partyCode: this.value},
       response => {
         console.log(response);
       });
   }
 
   async onCreatePartyButtonClick(): Promise<void> {
-    chrome.runtime.sendMessage(this.editorExtensionId, {event: 'createParty'},
+    chrome.runtime.sendMessage(this.extensionId, {event: 'createParty'},
       response => {
         console.log(response);
       });
