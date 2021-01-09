7f6c5dffa32fb6d3c87f9ee90f1cab1b086de09b
Fishuke <gamecalp@gmail.com>
2021-01-09 23:55:02 +0300

    Add beatifull codes

--- ytmparty-server/app.js
+++ ytmparty-server/app.js
@@ -8,6 +8,7 @@ const io = require('socket.io')({
 
 io.on('connection', (socket) => {
     console.log(socket.id + ' connected.');
+
     socket.on('createRoom', () => {
         console.log('createRoom');
         const roomID = nanoid.nanoid(8);
@@ -17,15 +18,45 @@ io.on('connection', (socket) => {
     })
 
     socket.on('joinRoom', (roomID) => {
-        if(socket.rooms.get(roomID).size === 0){
-            return socket.emit(error, 'Room not found or empty');
+        const rooms = io.sockets.adapter.rooms;
+
+        console.log(rooms);
+        console.log(rooms.get(roomID));
+        if (rooms.get(roomID)){
+            console.log(rooms.get(roomID).size);
         }
+
+
+        if (!io.sockets.adapter.rooms.get(roomID)) {
+            return socket.emit('error', 'Room doesnt exist.');
+        }
+
         socket.join(roomID);
         socket.emit('joinedRoom', roomID);
     })
 
-    socket.on('message', (msg) => {
-        console.log('message: ' + msg);
+    socket.on('leaveRoom', () => {
+        socket.leaveAll();
+    })
+
+    socket.on('pause', () => {
+        console.log('pause');
+        socket.broadcast.emit('pause');
+    });
+
+    socket.on('play', () => {
+        console.log('play');
+        socket.broadcast.emit('play');
+    });
+
+    socket.on('seeked', (to) => {
+        console.log('seeked');
+        socket.broadcast.emit('seeked', to);
+    });
+
+    socket.on('nextTrack', (data) => {
+        console.log('nextTrack');
+        socket.broadcast.emit('seeked', data);
     });
 });
 
