c41db6d8df186e5d8471b55dccc6df97f2b70857
Fishuke <gamecalp@gmail.com>
2021-01-09 23:55:04 +0300

    Add beatifull codes

--- ytmparty-plugin/src/app/app.module.ts
+++ ytmparty-plugin/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import {SnackbarService} from './services/snackbar.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [SnackbarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
