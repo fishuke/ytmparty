818ca37c2ec249206bb1cd2b0870cb876dc46b0c
Fishuke <fishukee@gmail.com>
2021-01-12 04:41:35 +0300

    Add cors multiple origin

--- ytmparty-server/app.js
+++ ytmparty-server/app.js
@@ -8,7 +8,7 @@ const server = express()
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: "chrome-extension://*",
+        origins: ["chrome-extension://oononiaicnkfdebjkpfabepkggkneeep", "chrome-extension://nlmcoiggngjmnocabgbgmojmclamjcel"],
         credentials: true
     }
 });
