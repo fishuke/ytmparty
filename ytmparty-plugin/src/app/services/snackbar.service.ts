a464ffd042011b5858397b92dac3c2b739b6bd85
Fishuke <fishukee@gmail.com>
2021-01-13 12:13:46 +0300

    Linting🔥

--- ytmparty-plugin/src/app/services/snackbar.service.ts
+++ ytmparty-plugin/src/app/services/snackbar.service.ts
@@ -10,42 +10,42 @@ export class SnackbarService {
   ) {
   }
 
-  default(message: string) {
+  default(message: string): void {
     this.show(message, {
       duration: 2000,
       panelClass: 'default-notification-overlay'
     });
   }
 
-  info(message: string) {
+  info(message: string): void {
     this.show(message, {
       duration: 2000,
       panelClass: 'info-notification-overlay'
     });
   }
 
-  success(message: string) {
+  success(message: string): void {
     this.show(message, {
       duration: 2000,
       panelClass: 'success-notification-overlay'
     });
   }
 
-  warn(message: string) {
+  warn(message: string): void {
     this.show(message, {
       duration: 2500,
       panelClass: 'warning-notification-overlay'
     });
   }
 
-  error(message: string) {
+  error(message: string): void {
     this.show(message, {
       duration: 3000,
       panelClass: ['error-notification-overlay'],
     });
   }
 
-  private show(message: string, configuration: MatSnackBarConfig) {
+  private show(message: string, configuration: MatSnackBarConfig): void {
     // Need to open snackBar from Angular zone to prevent issues with its position per
     // https://stackoverflow.com/questions/50101912/snackbar-position-wrong-when-use-errorhandler-in-angular-5-and-material
     this.zone.run(() => this.snackBar.open(message, null, configuration));
