98afe71cf3c5cef025bd3f18bb8595128361c1e5
fishuke <fishukee@gmail.com>
2022-03-11 01:57:07 +0300

    feat(api): remove rickroll

--- api/main.go
+++ api/main.go
@@ -14,17 +14,7 @@ func main() {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
-	m.HandleConnect(func(s *melody.Session) {
-		channel := s.Request.URL.String()[1:]
-
-		if contains(rickrollChannels, channel) {
-			s.Write([]byte(`{"event":"play","id":"dQw4w9WgXcQ"}`))
-		}
-
-	})
-
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		// dont send to sender
 		m.BroadcastFilter(msg, func(q *melody.Session) bool {
 			return q.Request.URL.Path == s.Request.URL.Path && q != s
 		})
